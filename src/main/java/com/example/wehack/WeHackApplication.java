f7e64af8b16868aa64150bbb2c12342ae35fcc3b
Zhangir05 <145916559+Zhangir05@users.noreply.github.com>
2024-05-05 11:26:00 +0500

    Initial commit

---
+++ src/main/java/com/example/wehack/WeHackApplication.java
@@ -0,0 +1,13 @@
+package com.example.wehack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeHackApplication.class, args);
+    }
+
+}
